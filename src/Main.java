5cf61aab2aa212bbe3c2476205bf8e929074ffd1
VeniaminBalan <99888346+VeniaminBalan@users.noreply.github.com>
2022-03-30 18:58:01 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        helloWorld( 10);
+    }
+
+    public static void helloWorld(int n)
+    {
+        for (int i=1; i<= n;i++) System.out.println(i+ " ");
+    }
+}
+
